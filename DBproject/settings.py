da768df841f41634c33ffb7a0441b5bf9403a4e2
linzichao <joy10393@gmail.com>
2018-05-16 14:23:56 +0800

    add UI

--- DBproject/settings.py
+++ DBproject/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Login Redirect Path
 LOGIN_REDIRECT_URL = '/'
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.views import login, logout
+import datetime
 
 # Create your views here.
 

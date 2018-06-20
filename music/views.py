30163ea35c84bccee522a0172039c02facf18ccc
Jeff <jeffrey110616@gmail.com>
2018-06-21 02:26:18 +0800

    finish front-end conncetion

--- music/views.py
+++ music/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.db import models
-from django.http import HttpResponseRedirect
-
+from django.http import HttpResponseRedirect, HttpResponse
+from music.models import Song, Album, Artist, Release, BelongTo
 # Create your views here.
+
+def search(request):
+    song, album, artist = request.GET['song'], request.GET['album'], request.GET['artist']
+    #songs = Song.objects.filter(SongName__icontains= song)
+    #albums = Album.objects.filter(AlbumName__icontains= album)
+    #artists = Artist.objects.filter(ArtistName__icontains= artist)
+    #AlbumID__AlbumName__icontains= album,
+    sql = """select * from music_song as SONG \
+                INNER JOIN music_belongto as BLON \
+                    on SONG.SongID = BLON.SongID_id \
+                INNER JOIN music_album as ALB \
+                    on BLON.AlbumID_id = ALB.AlbumID \
+                INNER JOIN music_release as REL \
+                    on ALB.AlbumID = REL.AlbumID_id \
+                INNER JOIN music_artist as ART \
+                    on REL.ArtistID_id = ART.ArtistID"""
+    songs = Song.objects.raw(sql)
+    for s in songs:
+        print("SongName: {}, AlbumName: {}, ArtistName: {}".format(s.SongName, s.AlbumName, s.ArtistName))
+    return HttpResponse("song = {}, album = {}, artist = {}".format(song, album, artist) )
+
 
 def index(request):
     username = None

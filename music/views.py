1ad703561091d87e82a7774bea49a6cc3af4718b
linzichao <joy10393@gmail.com>
2018-06-21 12:14:51 +0800

    add UI

--- music/views.py
+++ music/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.db import models
 from django.http import HttpResponseRedirect
 
+from music.models import Song, Album, Artist, BelongTo, Release
+
 # Create your views here.
 
 def index(request):
@@ -21,6 +23,19 @@ def playlist(request):
 def comment(request):
     if request.user.is_authenticated():
         if request.method == 'GET':
+
+            SongID = request.GET.get('songid', '')
+
+            ThisSongName = Song.objects.get(SongID=int(SongID)).SongName
+            ThisSongLyrics = Song.objects.get(SongID=int(SongID)).SongLyrics
+
+            SongBelongAlbum = BelongTo.objects.get(SongID_id=SongID)
+            ArtistReleaseAlbum = Release.objects.get(AlbumID=SongBelongAlbum.AlbumID_id)
+
+            ThisAlbumName = Album.objects.get(AlbumID=SongBelongAlbum.AlbumID_id).AlbumName
+            ThisArtistName = Artist.objects.get(ArtistID=ArtistReleaseAlbum.ArtistID_id).ArtistName
+
+
             return render(request, 'comment.html', locals())
     else:
         return HttpResponseRedirect("/accounts/login/")

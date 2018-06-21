afad05648eaac5c678ef213f6a965075a521ed27
Jeff <jeffrey110616@gmail.com>
2018-06-21 12:13:10 +0800

    search function

--- music/views.py
+++ music/views.py
@@ -6,11 +6,11 @@ from music.models import Song, Album, Artist, Release, BelongTo
 
 def search(request):
     song, album, artist = request.GET['song'], request.GET['album'], request.GET['artist']
-    #songs = Song.objects.filter(SongName__icontains= song)
-    #albums = Album.objects.filter(AlbumName__icontains= album)
-    #artists = Artist.objects.filter(ArtistName__icontains= artist)
-    #AlbumID__AlbumName__icontains= album,
-    sql = """select * from music_song as SONG \
+
+    song_like = "%%" if song == "" else "%%{}%%".format(song)
+    artist_like = "%%" if artist == "" else "%%{}%%".format(artist)
+    album_like = "%%" if album == "" else "%%{}%%".format(album)
+    sql = "select * from music_song as SONG \
                 INNER JOIN music_belongto as BLON \
                     on SONG.SongID = BLON.SongID_id \
                 INNER JOIN music_album as ALB \
@@ -18,12 +18,22 @@ def search(request):
                 INNER JOIN music_release as REL \
                     on ALB.AlbumID = REL.AlbumID_id \
                 INNER JOIN music_artist as ART \
-                    on REL.ArtistID_id = ART.ArtistID"""
-    songs = Song.objects.raw(sql)
-    for s in songs:
-        print("SongName: {}, AlbumName: {}, ArtistName: {}".format(s.SongName, s.AlbumName, s.ArtistName))
-    return HttpResponse("song = {}, album = {}, artist = {}".format(song, album, artist) )
+                    on REL.ArtistID_id = ART.ArtistID \
+            WHERE \
+                SONG.SongName LIKE \"{}\" AND \
+                ALB.AlbumName LIKE \"{}\" AND \
+                ART.ArtistName LIKE \"{}\" ".format(song_like, album_like, artist_like)
 
+    songs = Song.objects.raw(sql)
+
+    ls_return = []
+    for s in songs:
+        entry_tmp = "SongName: {}, AlbumName: {}, ArtistName: {}".format(s.SongName, s.AlbumName, s.ArtistName)
+        print(entry_tmp)
+        ls_return.append(entry_tmp)
+
+    #return HttpResponse("song = {}, album = {}, artist = {}".format(song, album, artist) )
+    return HttpResponse( ls_return )
 
 def index(request):
     username = None

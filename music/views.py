c4cc3b065f728c036005d8eca0d8ea42cbaa4f36
104703008 <104703008@nccu.edu.tw>
2018-06-30 03:41:21 +0800

    add playlist with song info API

--- music/views.py
+++ music/views.py
@@ -118,8 +118,24 @@ def get_my_playlist(request):
     if request.user.is_authenticated():
         if request.method == 'GET':
             x = PlayList.objects.filter(CreatedBy = request.user)
-            print(x)
             return JsonResponse({'data': list(x.values())})
+        else:
+            return HttpResponse('Bad Request', status=400)
+    else:
+        return HttpResponse('Unauthorized', status=401)
+
+def get_my_playlist_with_song_info(request):
+    if request.user.is_authenticated():
+        if request.method == 'GET':
+            playlists = PlayList.objects.filter(CreatedBy = request.user)
+            ret = []
+            for p in playlists:
+                a = AddTo.objects.filter(PlayListID = p.PlayListID)
+                print({ 'playlist_name': p.PlayListName
+                       , 'songs': list(Song.objects.filter(SongID__in = a).values()) })
+                ret.append( { 'playlist_name': p.PlayListName
+                       , 'songs': list(Song.objects.filter(SongID__in = a).values()) })
+            return JsonResponse({'data': ret})
         else:
             return HttpResponse('Bad Request', status=400)
     else:
